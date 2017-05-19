f53c34feaa6f2801b564ee8bed8d90ac7f43afb0
agavez <fredrikhoel89@gmail.com>
2017-05-19 14:08:44 +0200

    draggable background

---
+++ scripts/jquery/main-script.js
@@ -0,0 +1,29 @@
+$(function(){
+        
+    $("#backscreen")
+        .css(
+            {
+                width: "2000px",
+                height: "1500px",
+                backgroundColor: "green",
+                cursor: "pointer",
+                marginLeft: "-500px",
+                marginTop: "-250px"
+            }
+        )
+        .draggable(
+            {
+                start: function(){
+                    $(this)
+                },
+                drag: function(){
+                    $(this)
+                        .css("background-color", "darkgreen")
+                },
+                stop: function(){
+                    $(this)
+                        .css("background-color", "green")
+                }
+            }
+        )
+});
