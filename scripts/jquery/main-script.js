926b5130ff5f9fb199b652fdf19944f1333c5520
Agavez <fredrikhoel89@gmail.com>
2017-05-22 01:25:43 +0200

    Ikke alt funker, men funker ihvertfall

--- scripts/jquery/main-script.js
+++ scripts/jquery/main-script.js
@@ -3,31 +3,56 @@ $(function(){
     http://jsfiddle.net/UjztS/
     http://stackoverflow.com/questions/14316525/stop-dragging-when-edges-show-with-jquery
     */
-    $("#backscreen")
-        .css(
-            {
-                width: "300%",
-                height: "100%",
-                backgroundColor: "green",
-                cursor: "pointer",
-                marginLeft: "-500px"
-            }
-        )
-        .draggable(
-            {
-                containment: "parent",
-                start: function(){
-                    $(this)
-                },
-                drag: function(){
-                    $(this).css("background-color", "darkgreen")
-                },
-                stop: function(){
-                    $(this).css("background-color", "green")
-                }
-            }
-        )
+    //$("#backscreen")
+    //    .css(
+    //        {
+    //            width: "300%",
+    //            height: "100%",
+    //            backgroundColor: "green",
+    //            cursor: "pointer",
+    //            marginLeft: "-500px"
+    //        }
+    //    )
+    //    .draggable(
+    //        {
+    //            containment: "parent",
+    //            start: function(){
+    //                $(this)
+    //            },
+    //            drag: function(){
+    //                $(this).css("background-color", "darkgreen")
+    //            },
+    //            stop: function(){
+    //                $(this).css("background-color", "green")
+    //            }
+    //        }
+    //    )
     
+//set up width and height of draggable container according to view container and draggable size
+	var dragContainerWidth = $("#viewContainer").innerWidth() + ($('#draggable').outerWidth() - $("#viewContainer").innerWidth()) * 2;
+	
+	var dragContainerHeight = $("#viewContainer").innerHeight() + 
+	($('#draggable').outerHeight() - $("#viewContainer").innerHeight()) * 2;
+
+	$("#draggableContainer").css("width",dragContainerWidth);
+	$("#draggableContainer").css("height",dragContainerHeight);
+
+//set up position of draggable container according to view container and draggable size
+
+	var dragContainerOffsetLeft = $("#viewContainer").offset().left + $("#viewContainer").outerWidth()/2 + $("#viewContainer").innerWidth()/2 - $('#draggable').outerWidth();
+	
+	var dragContainerOffsetTop = $("#viewContainer").offset().top + $("#viewContainer").outerHeight()/2 + $("#viewContainer").innerHeight()/2 - $('#draggable').outerHeight();
+
+	$("#draggableContainer").offset({left:dragContainerOffsetLeft,top:dragContainerOffsetTop});
+
+
+
+//activate draggable
+	$('#draggable').draggable({containment:"#draggableContainer"});
+	
+	
+	
+	
     $(".images").on(
         {
             click: function(){
